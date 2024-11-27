7f0281fb4afe15ce5d5d570a2e739f3fdc2b23f3
raspberry-pi <raspberry-pi@kakao.com>
2024-11-27 15:49:56 +0900

    WIP

---
+++ gatle.py
@@ -0,0 +1,32 @@
+import random
+from datetime import date
+
+from quantum_simulator import quantum_circuit
+
+def set_day_seed():
+    today = date.today().strftime("%Y%m%d")
+    random.seed(int(today))
+
+def set_game() -> tuple[int]:
+    set_day_seed()
+    n_bit = random.randint(4,8)
+    answer = random.randint(1, 2 ** n_bit) - 1
+
+    return {
+        'n_bits': n_bit, 
+        'answer' : answer
+        }
+    
+def play_game(data):
+    n_bit, answer = set_game()
+    
+    random.seed()
+    observed = quantum_circuit(data)
+    
+    bin_answer = bin(answer)[2:]
+    bin_answer = [int(digit) for digit in bin_answer]
+    
+    
+    
+if __name__ == '__main__':
+    set_game()
